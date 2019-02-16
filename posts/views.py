af58ea83ddd1dac9c45a2733d557af9a25a820d2
shahanursharif <sharif@microdreamit.com>
2019-02-16 19:59:15 +0600

    edit, create, show,

--- posts/views.py
+++ posts/views.py
@@ -5,6 +5,7 @@ from posts.forms import PostForm
 from posts.models import Post
 
 
+# public method
 def index(request):
     posts = Post.objects.order_by('-created_at')
     return render(request, 'index.html', {'posts': posts})
@@ -14,9 +15,7 @@ def create(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
-            instance = form.save(commit=False)
-            instance.save()
-            messages.success(request, 'blog has been created')
+            __post_save(form, request, 'blog has been created')  # sub method
         else:
             messages.error(request, 'please try again')
             return render(request, 'create.html', {'form': form})
@@ -24,30 +23,32 @@ def create(request):
     return render(request, 'create.html', {'form': PostForm})
 
 
-# Create your views here.
 def show(request, id):
     post = get_object_or_404(Post, id=id)
     return render(request, 'show.html', {'post': post})
 
 
-# Create your views here.
 def edit(request, id):
     post = get_object_or_404(Post, id=id)
     form = PostForm(request.POST or None, instance=post)
     if request.method == 'POST':
         if form.is_valid():
-            instance = form.save(commit=False)
-            instance.save()
-            messages.success(request, 'blog has been updated')
+            __post_save(form, request, 'blog has been updated')  # sub method
         else:
             messages.error(request, 'Please try again')
 
     return render(request, 'edit.html', {'form': form, 'post': post})
 
 
-# Create your views here.
 def delete(request, id):
     instance = get_object_or_404(Post, id=id)
     instance.delete()
     messages.success(request, 'successfully deleted')
     return redirect('posts:index')
+
+
+# private methods
+def __post_save(form, request, message):
+    instance = form.save(commit=False)
+    instance.save()
+    messages.success(request, message)
