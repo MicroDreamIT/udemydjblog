a5c7bdde6c8f93e56180d7f070dbd6e1768e9f67
shahanursharif <sharif@microdreamit.com>
2019-02-18 21:15:21 +0600

    comment model changes

--- comments/models.py
+++ comments/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 from django.conf import settings
-
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 # Create your models here.
 from posts.models import Post
 
 
 class Comment(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, default=1, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
+    # post = models.ForeignKey(Post, default=1, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, null=True)
+    object_id = models.PositiveIntegerField(null=True)
+    content_object = GenericForeignKey('content_type', 'object_id')
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
